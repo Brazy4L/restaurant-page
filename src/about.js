405fdfd33c9598c47ecf722253accf8f3eb19948
Brazy <shutn1q@gmail.com>
2022-08-06 15:56:53 +0300

    Add about page

--- src/about.js
+++ src/about.js
@@ -4,6 +4,15 @@ const about = () => {
     let content = document.createElement('div');
     content.id = 'content-about';
     wrapper.appendChild(content);
+    let div = document.createElement('div');
+    div.innerText = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste iusto quisquam, hic aspernatur rem harum provident. Sed, accusamus. Provident porro quod esse libero fugiat ratione, ipsam iusto repellendus minus quas eos magnam facilis pariatur nobis velit atque dignissimos consectetur odit maxime debitis nisi obcaecati nulla ad exercitationem! Aspernatur deserunt itaque veritatis sapiente et exercitationem ut, tempore ipsum ducimus, sequi eos fugiat similique minima architecto distinctio nostrum cum, non asperiores ad corrupti saepe id illo amet ipsam. Obcaecati fugiat harum quis facilis eum impedit, debitis distinctio soluta totam fugit quaerat nulla sed! Aperiam, eligendi. Quasi architecto aspernatur suscipit consequatur dolore facere.';
+    content.appendChild(div);
+    let div2 = document.createElement('div');
+    div2.innerText = 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eaque non at illum iure voluptate vero aliquid sint? Deserunt qui aspernatur adipisci sed, corporis quos ab culpa veniam recusandae fugit odio? Reprehenderit, possimus? Animi tempore temporibus, rem magnam, ullam adipisci illo commodi quidem atque suscipit optio mollitia facilis fuga ipsa nam harum at reiciendis. Incidunt fuga, facilis magni harum unde ea quidem ratione libero sit optio itaque molestias velit dolores voluptatum nostrum neque iure fugit aspernatur voluptate, eligendi deserunt deleniti architecto repellendus. Eius, doloremque totam sequi fugiat eum facilis animi dolor quo vero saepe vel deserunt dolorem explicabo illum. Excepturi, dicta.';
+    content.appendChild(div2);
+    let div3 = document.createElement('div');
+    div3.innerText = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis voluptate maiores tempora enim eius commodi, itaque quibusdam fuga at, nesciunt perspiciatis aperiam temporibus sit suscipit, veniam impedit. Reiciendis obcaecati totam consequuntur mollitia, eos culpa ipsum corrupti ratione neque at? Quidem mollitia consequatur a fuga expedita. Voluptatem iure qui autem ab corporis, cumque perspiciatis dolor officiis sit quam accusamus, quod voluptatibus, assumenda distinctio provident! Magnam quod expedita praesentium, suscipit quo eius quasi ad unde explicabo molestias perspiciatis odit iste rerum in illo doloremque nesciunt alias sit? Iusto, et sunt? Accusantium a animi cupiditate odio nemo debitis? Quaerat porro voluptate modi facilis.';
+    content.appendChild(div3);
 }
 
 export default about;
