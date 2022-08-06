7f953d7509c8c661223541e459ef08ab175222cd
Brazy <shutn1q@gmail.com>
2022-08-06 23:33:17 +0300

    Add active page

--- src/index.js
+++ src/index.js
@@ -5,15 +5,19 @@ import about from './about';
 
 if (localStorage.getItem('page') === 'main') {
     headerFooter()
+    document.querySelector('#header > :nth-child(1)').classList.add('active');
     theRestaurant();
 } else if (localStorage.getItem('page') === 'menu') {
     headerFooter()
+    document.querySelector('#header > :nth-child(2)').classList.add('active');
     menu();
 } else if (localStorage.getItem('page') === 'about') {
     headerFooter()
+    document.querySelector('#header > :nth-child(3)').classList.add('active');
     about();
 } else {
     headerFooter()
+    document.querySelector('#header > :nth-child(1)').classList.add('active');
     theRestaurant();
 }
 
@@ -23,6 +27,9 @@ const pageChanger = () => {
     document.querySelector('#header > :nth-child(1)').addEventListener('click', () => {
         wrapper.innerHTML = '';
         headerFooter();
+        document.querySelector('#header > :nth-child(1)').classList.add('active');
+        document.querySelector('#header > :nth-child(2)').classList.remove('active');
+        document.querySelector('#header > :nth-child(3)').classList.remove('active');
         theRestaurant();
         localStorage.setItem('page', 'main');
         pageChanger();
@@ -31,6 +38,9 @@ const pageChanger = () => {
     document.querySelector('#header > :nth-child(2)').addEventListener('click', () => {
         wrapper.innerHTML = '';
         headerFooter();
+        document.querySelector('#header > :nth-child(2)').classList.add('active');
+        document.querySelector('#header > :nth-child(1)').classList.remove('active');
+        document.querySelector('#header > :nth-child(3)').classList.remove('active');
         menu();
         localStorage.setItem('page', 'menu');
         pageChanger();
@@ -39,6 +49,9 @@ const pageChanger = () => {
     document.querySelector('#header > :nth-child(3)').addEventListener('click', () => {
         wrapper.innerHTML = '';
         headerFooter();
+        document.querySelector('#header > :nth-child(3)').classList.add('active');
+        document.querySelector('#header > :nth-child(1)').classList.remove('active');
+        document.querySelector('#header > :nth-child(2)').classList.remove('active');
         about();
         localStorage.setItem('page', 'about');
         pageChanger();
